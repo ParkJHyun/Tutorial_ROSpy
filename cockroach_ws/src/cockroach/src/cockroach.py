264aab8514e536e85a680c17e3b2d70158d0237a
ParkJaeHyun <xoxc4565@naver.com>
2018-08-10 21:05:30 +0900

    cockroach

--- cockroach_ws/src/cockroach/src/cockroach.py
+++ cockroach_ws/src/cockroach/src/cockroach.py
@@ -6,9 +6,9 @@ from sensor_msgs.msg import LaserScan
 def scan_callback(msg):
 	global g_range_ahead
 	g_range_ahead = min(msg.ranges)
-	print g_range_ahead
 	
-g_range_ahead = 1
+	
+g_range_ahead = 1.2
 scan_sub = rospy.Subscriber('scan',LaserScan,scan_callback)
 cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 
@@ -21,26 +21,31 @@ go_forward = "GO FORWARD!"
 turn = "Turn!"
 twist = Twist()
 
+def go_back():
+	twist=Twist()
+	twist.linear.x= -1
+
 while not rospy.is_shutdown():
-	print "%d" % driving_forward
 	if driving_forward :
-		if (g_range_ahead < 0.6 or rospy.Time.now() > state_change_time):
+		if (g_range_ahead < 1.1 and rospy.Time.now() > state_change_time):
 			driving_forward = False
 			state_change_time = rospy.Time.now() + rospy.Duration(2)
-	else:
-		if rospy.Time.now() > state_change_time :
-			driving_foward = True
-			state_change_time = rospy.Time.now() + rospy.Duration(2)
-		else :
-			print "else!"
+		elif (g_range_ahead < 0.5 or g_range_ahead > 5) :
+			go_back()
 	
+	else :
+		if rospy.Time.now() > state_change_time :
+			driving_forward = True
+			state_change_time = rospy.Time.now() + rospy.Duration(10)
 	
 	if driving_forward:
 		twist.linear.x = 0.2
-		rospy.loginfo(go_forward)
+		twist.angular.z=0
+		
 	else:
-		twist.angular.z = 0.5
-		print "Trun!!"
+		twist.angular.z = 0.8
+		twist.linear.x=0
+		
 
 	cmd_vel_pub.publish(twist)
 
